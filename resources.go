39a09ec93940cf848439baf2fb4331942f9dd249
stack72 <public@paulstack.co.uk>
2019-07-25 11:05:51 -0700

    Run tfgen with latest pulumi-terraform

--- resources.go
+++ resources.go
@@ -98,7 +98,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "latest",
+				"@pulumi/pulumi": "^0.17.25",
 			},
 			DevDependencies: map[string]string{
 				"@types/node": "^8.0.25",
